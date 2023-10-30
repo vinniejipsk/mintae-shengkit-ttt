32d4730a2fd73e4a4894c8dec4a6e656c11a076c
vinniejipsk <141219700+vinniejipsk@users.noreply.github.com>
2023-10-30 17:57:41 +0800

    Change to BoardRender.

--- tictactoe_react/src/App.jsx
+++ tictactoe_react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import ScoreBoardRender from './ScoreBoardRender'
+import BoardRender from './BoardRender'
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className = 'App'>
-        <ScoreBoardRender />
+        <BoardRender />
       </div>
     </>
   )
